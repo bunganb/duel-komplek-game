0140195d6423b0f4e45ad6f56b69dcf20dbd2eb1
bung_anb <bunganabila1005@gmail.com>
2025-06-14 19:59:50 +0700

    feat : add health bar

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -1,25 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthController : MonoBehaviour
 {
-    public int maxHealth = 10;
+    public int maxHealth = 100;
     [SerializeField] private int currentHealth;
     private int damage = 10;
+
+    public Image healthFill; 
 
     private void Awake()
     {
         currentHealth = maxHealth;
+        UpdateHealthUI();
     }
     
     public void TakeDamage()
     {
         currentHealth -= damage;
+        if (currentHealth < 0) currentHealth = 0;
+
+        UpdateHealthUI();
+
         if (currentHealth == 0)
         {
-            currentHealth = 0;
             Debug.Log(gameObject.name + " defeated!");
         }
+    }
+  
+    private void UpdateHealthUI()
+    {
+        healthFill.fillAmount = (float)currentHealth / maxHealth;
     }
 }
